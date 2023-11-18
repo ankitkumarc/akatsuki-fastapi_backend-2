0a3c7fc383e415fe4387c0a6388286726625a898
Ankit kumar <ankitkumarcode24@gmail.com>
2023-11-19 02:01:51 +0530

    purchase-year-month

--- app/api/api_v1/handlers/purchase.py
+++ app/api/api_v1/handlers/purchase.py
@@ -9,10 +9,21 @@ from uuid import UUID
 from typing import List
 
 purchase_router = APIRouter()
+
 @purchase_router.get('/{customer_id}', summary="Get All Purchases", response_model=List[Purchase])
 async def list_purchases(customer_id: UUID):
     purchases = await PurchaseService.list_purchases(customer_id)
     return purchases
+
+@purchase_router.get('/year/{customer_id}', summary="Get purchases by year", response_model=List[Purchase])
+async def list_purchases_by_year(customer_id : UUID, year: int):
+        purchases = await PurchaseService.list_purchases_by_year(customer_id, year)
+        return purchases
+
+@purchase_router.get('/month/{customer_id}', summary="Get purchases by year", response_model=List[Purchase])
+async def list_purchases_by_year(customer_id : UUID, month: int):
+        purchases = await PurchaseService.list_purchases_by_month(customer_id, month)
+        return purchases
 
 @purchase_router.post('/', summary="Create Purchase", response_model=Purchase)
 async def create_purchase(data: PurchaseCreate):
--- app/services/purchase_service.py
+++ app/services/purchase_service.py
@@ -1,6 +1,7 @@
+import datetime
 from uuid import UUID
 from typing import List
-
+from datetime import datetime
 from beanie import init_beanie
 from fastapi import HTTPException, status
 from app.models.purchase_model import Purchase
@@ -14,6 +15,24 @@ class PurchaseService:
         if purchases:
             return purchases
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="purchase not found")
+    
+    @staticmethod
+    async def list_purchases_by_year(customer_id: UUID, year: int) -> List[Purchase]:
+        purchases = await Purchase.find(Purchase.customer_id == customer_id).to_list()
+        filtered_purchases = [
+            purchase for purchase in purchases
+            if purchase.customer_id == customer_id and purchase.created_at.year == year
+        ]
+        return filtered_purchases
+    
+    @staticmethod
+    async def list_purchases_by_month(customer_id: UUID, month: int) -> List[Purchase]:
+        purchases = await Purchase.find(Purchase.customer_id == customer_id).to_list()
+        filtered_purchases = [
+            purchase for purchase in purchases
+            if purchase.customer_id == customer_id and purchase.created_at.month == month
+        ]
+        return filtered_purchases
     
     @staticmethod
     async def create_purchase( data: PurchaseCreate) -> Purchase:
